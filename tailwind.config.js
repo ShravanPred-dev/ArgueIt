b69e6bc1054868ab67edf41a96b272586742397e
Steve Mendeleev <63888814+StiopaPopa@users.noreply.github.com>
2021-02-28 17:59:26 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "custom-blue": "#1E96FC",
+        "custom-white": "#FCF7F8",
+        "custom-red": "#F61067",
+        "custom-green": "#06D6A0",
+        "custom-black": "#39393A",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
